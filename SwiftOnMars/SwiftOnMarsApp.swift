39a8c9613ca4953c0e459ac193e48823955b7a5d
Martin <mlukacs.dev@gmail.com>
2023-04-10 17:28:10 +0200

    Initial Commit

---
+++ SwiftOnMars/SwiftOnMarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftOnMarsApp.swift
+//  SwiftOnMars
+//
+//  Created by Martin Lukacs on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftOnMarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
